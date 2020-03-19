fbdec90f5c4ee45c2d1c6b48df496663c2c44440
1900608 Elliot Stevenson <1900608@uad.ac.uk>
2020-03-19 10:30:47 +0000

    Create Zombie.h

---
+++ Week5/CMP105App/Zombie.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Framework/GameObject.h"
+#include "Framework/Animation.h"
+class Zombie : public GameObject
+{
+public:
+	Zombie();
+	~Zombie();
+
+	void update(float dt) override;
+	void handleInput(float dt) override;
+protected:
+	Animation walk;
+};
+
