0a8a7ab6c783569f1a21841aedc99f966896dfb6
1900608 Elliot Stevenson <1900608@uad.ac.uk>
2020-03-19 10:30:37 +0000

    Update Level.h

--- Week5/CMP105App/Level.h
+++ Week5/CMP105App/Level.h
@@ -3,11 +3,13 @@
 #include <SFML/Graphics.hpp>
 #include "Framework/Input.h"
 #include "Framework/GameObject.h"
+#include "Zombie.h"
+#include "Mario.h"
 #include <string>
 #include <iostream>
 
 
-class Level{
+class Level {
 public:
 	Level(sf::RenderWindow* hwnd, Input* in);
 	~Level();
@@ -20,8 +22,24 @@ private:
 	// Default functions for rendering to the screen.
 	void beginDraw();
 	void endDraw();
+	int MoveHiddenX = 0;
+	int MoveHiddenY = 0;
 
 	// Default variables for level class.
 	sf::RenderWindow* window;
 	Input* input;
+
+	//level objects 
+	sf::Texture ZombieText;
+	sf::Texture MarioText;
+	sf::Texture transparent;
+	Zombie ZombieTest;
+	Mario testMario;
+	sf::CircleShape cursour;
+	sf::RectangleShape coverup;
+	sf::RectangleShape coverdown;
+	sf::RectangleShape coverright;
+	sf::RectangleShape coverleft;
+	sf::Font font;
+	sf::Text information;
 };
