38c9b553f203e5b9e1d5deb679546bfdaf4fddc1
1900608 Elliot Stevenson <1900608@uad.ac.uk>
2020-04-07 20:34:29 +0100

    Update Zombie.cpp

--- Week5/CMP105App/Zombie.cpp
+++ Week5/CMP105App/Zombie.cpp
@@ -10,6 +10,7 @@ Zombie::Zombie()
 	walk.addFrame(sf::IntRect(275, 0, 55, 108));
 	walk.addFrame(sf::IntRect(330, 0, 55, 108));
 	walk.addFrame(sf::IntRect(385, 0, 55, 108));
+
 	walk.setFrameSpeed(1.f / 10.f);
 }
 
@@ -17,41 +18,48 @@ Zombie::~Zombie()
 {
 
 }
+
 void Zombie::update(float dt)
 {
 
 	setTextureRect(walk.getCurrentFrame());
-
+	walk.animate(dt);
 }
 
 void Zombie::handleInput(float dt)
 {
 	if (input->isKeyDown(sf::Keyboard::W))
 	{
+		walk.setFrameSpeed(1.f / 10.f);
 		setVelocity(0, -100);
 		move(velocity * dt);
 		walk.animate(dt);
-
+		setTextureRect(walk.getCurrentFrame());
 	}
 	if (input->isKeyDown(sf::Keyboard::S))
 	{
+		walk.setFrameSpeed(1.f / 10.f);
 		setVelocity(0, 100);
 		move(velocity * dt);
 		walk.animate(dt);
+		setTextureRect(walk.getCurrentFrame());
 	}
 	if (input->isKeyDown(sf::Keyboard::A))
 	{
+		walk.setFrameSpeed(1.f / 10.f);
 		setVelocity(-100, 0);
 		move(velocity * dt);
 		walk.setFlipped(true);
 		walk.animate(dt);
+		setTextureRect(walk.getCurrentFrame());
 	}
 	if (input->isKeyDown(sf::Keyboard::D))
 	{
+		walk.setFrameSpeed(1.f / 10.f);
 		setVelocity(100, 0);
 		move(velocity * dt);
 		walk.setFlipped(false);
 		walk.animate(dt);
+		setTextureRect(walk.getCurrentFrame());
 	}
-
 }
